d825073d3a369dfc03a65670e1fe13bf92a4d1fa
olesh11 <romanolesh2@gmail.com>
2026-04-22 23:08:33 +0300

    fix8

--- lab_2_react/vite.config.js
+++ lab_2_react/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default {
+  css: {
+    transformer: 'postcss' // 👈 це ключове
+  }
+}
